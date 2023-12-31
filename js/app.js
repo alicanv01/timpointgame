55e49e50296d08cac3b7f4e3731e6f1115b7acdb
alican1809 <94050902+alican1809@users.noreply.github.com>
2023-12-31 04:25:41 +0100

    a

--- js/app.js
+++ js/app.js
@@ -1,43 +1,74 @@
 let lineA = document.querySelectorAll("#lineA");
-console.log("c la ligne A" , lineA);
-let caseLineA =document.querySelectorAll('.caseLineA')
-console.log("c les caseLigne A" , caseLineA);
-console.log("c les caseAB" , caseLineA[0]);
+console.log("c la ligne A", lineA);
+let caseLineA = document.querySelectorAll('.caseLineA')
+console.log("c les caseLigne A", caseLineA);
+console.log("c les caseAB", caseLineA[0]);
 
-
-//recupere les resulta
 let resultArray = []
-for (let i = 0; i < caseLineA.length; i++) {
+
+function play() {
+     for (let i = 0; i < caseLineA.length; i++) {
     let dataValue = caseLineA[i].getAttribute('data');
     resultArray.push(dataValue);
 }
 console.log(resultArray)
 
-//comparer les resultat
-let result = resultArray.every(function(element){
+let result = resultArray.every(function (element) {
     return element === resultArray[0];
 })
 
-let amountPointDiv=0;
-let symbolAValue=10;
-let symbolBValue=3;
-let symbolCValue=1;
-function gain(){
+
+    // Select the checked radio button
+    let spinValueChoix = document.querySelector('input[name="spinValueChoix"]:checked');
+
+    // Check if any radio button is selected
+    if (spinValueChoix) {
+      // Retrieve and log the value of the selected radio button
+      var spinValue = spinValueChoix.value;
+      console.log("Selected Radio Value:", spinValue);
+    } else {
+      console.log("No radio button selected");
+    }
+
+
+
+
+
+
+
+
+
+
+
+console.log(spinValue)
+let sold = 500;
+let symbolAValue = spinValue * 10;
+let symbolBValue = spinValue * 3;
+let symbolCValue = spinValue * 1;
+
 if (result) {
     console.log("win")
-
-    if( resultArray[0]=="a"){
-       amountPointDiv = symbolAValue+amountPointDiv;
-        console.log("vous gangé ", symbolAValue , " votre sold est de " , amountPointDiv)
+    if (1==1) {
+        console.log("fzef")
+        sold = symbolAValue + sold - spinValue;
+        console.log("vous gangé ", symbolAValue, " votre sold est de ", sold)
     }
 } else {
     console.log("lose")
 }
+
+
+
+resultArray=[]
 }
 
 
 
+const playButonGame=document.getElementById("playButonGame")
+console.log(play)
 
 
 
+   
 
+playButonGame.addEventListener('click',play );
